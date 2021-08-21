165ecfd7a27e16066d007caafd1cc0879736f8b5
Samuel Tambunan <tambunansamuel@gmail.com>
2021-08-21 12:05:18 +1000

    Add project files.

---
+++ ExampleServices/IMathService.cs
@@ -0,0 +1,10 @@
+﻿namespace Spt.Examples.ExampleServices
+{
+	public interface IMathService
+	{
+		/// <summary>
+		/// Adds two integers together
+		/// </summary>
+		int Add(int a, int b);
+	}
+}
---
+++ ExampleServices/MathService.cs
@@ -0,0 +1,18 @@
+﻿namespace Spt.Examples.ExampleServices
+{
+	public class MathService : IMathService
+	{
+		public int Add(int a, int b)
+		{
+			return a + b;
+		}
+	}
+
+	public interface IMathService
+	{
+		/// <summary>
+		/// Adds two integers together
+		/// </summary>
+		int Add(int a, int b);
+	}
+}
