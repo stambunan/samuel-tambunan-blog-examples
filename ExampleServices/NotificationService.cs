fd20e77437a5722d2bbc991f29fdd7fa73255941
Samuel Tambunan <tambunansamuel@gmail.com>
2021-08-21 12:39:34 +1000

    Add unit test

---
+++ ExampleServices/NotificationService.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExampleServices
+{
+	public class NotificationService
+	{
+		private IEmailService _emailService;
+
+		public NotificationService(IEmailService emailService)
+		{
+			_emailService = emailService;
+		}
+		public void NotifyUser(User user)
+		{
+			if (user.NeedsNotification)
+			{
+				_emailService.SendEmail(user);
+			}
+		}
+	}
+
+	public interface IEmailService
+	{
+		void SendEmail(User user);
+	}
+
+	public class User
+	{
+		public string FirstName { get; set; }
+		public bool NeedsNotification { get; set; }
+	}
+}
---
+++ NUnitTesting/NotificationTests.cs
@@ -0,0 +1,44 @@
+﻿using ExampleServices;
+using Moq;
+using NUnit.Framework;
+
+namespace NUnitTesting
+{
+	[TestFixture]
+	public class NotificationTests
+	{
+		private NotificationService _serviceToTest;
+		private Mock<IEmailService> _mockEmailService;
+
+		[SetUp]
+		public void SetUp()
+		{
+			_serviceToTest = new NotificationService(_mockEmailService.Object);
+		}
+
+		[Test]
+		public void Ensure_NotifyUser_sends_email_when_user_needs_notification()
+		{
+			// Arrange
+			User userToAssert = null;
+
+			_mockEmailService.Setup(x => x.SendEmail(It.IsAny<User>()))
+				.Callback<User>(y => userToAssert = y);
+
+			// Act
+			var userToCheck = new User() { FirstName = "Samuel", NeedsNotification = true };
+			_serviceToTest.NotifyUser(userToCheck);
+
+			//Assert
+			_mockEmailService.Verify(x=> x.SendEmail(It.IsAny<User>()), Times.Once, "A user that needs notification should be emailed");
+			
+			// First way of verifying the properties in the user
+			_mockEmailService.Verify(x=> x.SendEmail(It.Is<User>(u => u.NeedsNotification == true && u.FirstName == "Samuel")));
+
+			// Second way of verifying the properties in the user
+			Assert.True(userToAssert.NeedsNotification);
+			Assert.AreEqual("Samuel", userToAssert.FirstName);
+
+		}
+	}
+}
